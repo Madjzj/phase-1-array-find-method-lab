bf52f10d616c64aaad51862f77b14903e9cb0eca
JakobJohnson <madjzj@gmail.com>
2023-08-12 13:51:31 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function superbowlWin(record){
+    const win = record.find(findWin);
+    if(typeof win === "object"){
+        return win.year;
+    } else {
+        return;
+    }
+    
+}
+function findWin(element, index, array){
+    if(element.result === "W"){
+        const winYear = element
+        return winYear;
+    }
+
+}
